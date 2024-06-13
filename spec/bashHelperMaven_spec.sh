965a271c8c46efd8074f441867b446385667fc69
hoobert <bluewhobert@mailbox.org>
2024-06-13 22:11:16 +0200

    TDD: add failing tests (empty output cases)

--- spec/bashHelperMaven_spec.sh
+++ spec/bashHelperMaven_spec.sh
@@ -1,10 +1,49 @@
 Describe "bashHelperMaven.sh"
   Include ./bashHelperMaven.sh
 
+  # these functions are workarounds, ShellSpec does not allow piped oparations in its When statements
+  catAndCallShowOnlyDownGradeLines() {
+      cat "${SHELLSPEC_PROJECT_ROOT}/spec/testdata/$1MavenDependencyTreeOutput.txt" | showOnlyDownGradeLines
+      return $?
+  }
+  catDowngradeAndCallShowOnlyDownGradeLines() {
+    catAndCallShowOnlyDownGradeLines "downgrade"
+    return $?
+  }
+  catFullAndCallShowOnlyDownGradeLines() {
+    catAndCallShowOnlyDownGradeLines "full"
+    return $?
+  }
+  catSameVersionAndCallShowOnlyDownGradeLines() {
+    catAndCallShowOnlyDownGradeLines "sameVersion"
+    return $?
+  }
+  catUnrelatedAndCallShowOnlyDownGradeLines() {
+    catAndCallShowOnlyDownGradeLines "unrelated"
+    return $?
+  }
+  catUpgradeAndCallShowOnlyDownGradeLines() {
+    catAndCallShowOnlyDownGradeLines "upgrade"
+    return $?
+  }
+
   Describe "showOnlyDownGradeLines"
-    It "should exist"
-      When I call showOnlyDownGradeLines
-      The status should be success
+    Describe "ignored content"
+      It "should ignore unrelated output lines"
+        When I call catUnrelatedAndCallShowOnlyDownGradeLines
+        The status should be success
+        The output should equal ""
+      End
+      It "should ignore upgrade output lines"
+        When I call catUpgradeAndCallShowOnlyDownGradeLines
+        The status should be success
+        The output should equal ""
+      End
+      It "should ignore same-version output lines"
+        When I call catSameVersionAndCallShowOnlyDownGradeLines
+        The status should be success
+        The output should equal ""
+      End
     End
   End
 End
