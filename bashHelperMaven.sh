9b3315813a47c2c254d626d1b176935736dac3e6
hoobert <bluewhobert@mailbox.org>
2024-06-13 22:45:46 +0200

    WIP: implement line parsing

--- bashHelperMaven.sh
+++ bashHelperMaven.sh
@@ -3,6 +3,36 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+function isDowngrade() {
+    local versionFrom
+    versionFrom="$1"
+    local versionTo
+    versionTo="$2"
+
+    # TODO compare versions
+    echo "NO"
+}
+
+function printOutputIfDowngradeLine() {
+    local line
+    local library
+    local versionFrom
+    local versionTo
+
+    line="$1"
+    # [INFO]    |  +- (net.bytebuddy:byte-buddy:jar:1.14.12:test - version managed from 1.14.16; omitted for duplicate)
+    # [INFO]    |  +- net.bytebuddy:byte-buddy-agent:jar:1.13.16:test (version managed from 1.14.12)
+
+    # not using sed, because it's not portable on macOS
+    library=$(echo "$line" | grep -Eo "+- \(?[a-z0-9.-]*:[a-z0-9.-]*:jar" | grep -Eo "[a-z0-9.-]+:[a-z0-9.-]+")
+    versionFrom=$(echo "$line" | grep -Eo "version managed from [0-9]+\.[0-9]+\.[0-9]+[;)]" | grep -Eo "[0-9]+\.[0-9]+\.[0-9]+")
+    versionTo=$(echo "$line" | grep -Eo ":jar:[0-9]+\.[0-9]+\.[0-9]+:(compile|test)" | grep -Eo "[0-9]+\.[0-9]+\.[0-9]+")
+
+    if [ $(isDowngrade "$versionFrom" "$versionTo") == "YES" ]; then
+      echo "$library \t downgraded from $versionFrom to $versionTo"
+    fi
+}
+
 function showOnlyDownGradeLines() {
     if [ -t 0 ]; then
         # called "un-piped"
@@ -15,8 +45,7 @@ function showOnlyDownGradeLines() {
     fi
     # consume stdin
     while read -r line; do
-        # do nothing
-        true
+        printOutputIfDowngradeLine "$line"
     done
     return 0
 }
